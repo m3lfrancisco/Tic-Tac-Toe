3ce41ae90e181ebb04c71ae09260ea4d09b28e3c
mfrancisco <francisco.melanie@gmail.com>
2022-04-03 23:05:06 -0700

    initializing board

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+
+let board;
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+function initialize() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    function render() {
+        board.forEach(function(mark, index) {
+            console.log(mark, index);
+        });
+    };
+    render();
+};
+
+initialize();

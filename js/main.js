daaabf034921124e5ed17a3768b1e0afa0dd1430
mfrancisco <francisco.melanie@gmail.com>
2022-04-03 23:37:01 -0700

    event listener working, click on box logging an 'X' on the right index on console

--- js/main.js
+++ js/main.js
@@ -3,18 +3,35 @@
 
 let board;
 
+let turn = 'X'
+
 /*----- cached element references -----*/
+
+const boxArray = Array.from(document.querySelectorAll('#board div'));
+
 /*----- event listeners -----*/
+
+document.getElementById('board').addEventListener('click', handleTurn);
+
 /*----- functions -----*/
 
 function initialize() {
     board = ['', '', '', '', '', '', '', '', ''];
     function render() {
         board.forEach(function(mark, index) {
-            console.log(mark, index);
+            // console.log(mark, index);
+            boxArray[index].textContent = mark;
         });
     };
     render();
 };
 
 initialize();
+
+function handleTurn(event) {
+    let idx = boxArray.findIndex(function(box) {
+        return box === event.target;
+    });
+        board[idx] = turn;
+        console.log(board);   
+};
